d9f7d663bd506baec6818d410e3d79fb11dcca5a
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-04-12 15:22:33 -0700

    Update SELECTnames.sql

--- SELECTnames.sql
+++ SELECTnames.sql
@@ -71,3 +71,12 @@ SELECT capital, name
 FROM world
 WHERE capital LIKE '%' + name + '%';
 
+-- 14. Find the capital and the name where the capital is an extension of name of the country.
+-- You should include Mexico City as it is longer than Mexico. You should not include Luxembourg as the capital is the same as the country.
+SELECT capital, name
+FROM world
+WHERE capital LIKE name + '_%';
+
+-- 15. For Monaco-Ville the name is Monaco and the extension is -Ville.
+-- Show the name and the extension where the capital is an extension of name of the country.
+-- You can use the SQL function REPLACE.
