edca10eb98a8fd36d17cdda933203c4f1fef1b7c
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-04-13 12:49:07 -0700

    Update SELECTworld.sql

--- SELECTworld.sql
+++ SELECTworld.sql
@@ -62,4 +62,10 @@ SELECT name, ROUND(population/1000000, 2) AS population,
 FROM world
 WHERE continent = 'South America';
 
+-- 10. Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). 
+-- Round this value to the nearest 1000.
+-- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+SELECT name, ROUND(gdp/population, -3) AS 'GDP per capita'
+FROM world
+WHERE gdp >= 1000000000000;
 
