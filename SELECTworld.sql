1b7bdd86575fe69e0a4c2a5a45347d8ac5a9c665
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-04-13 11:18:24 -0700

    Update SELECTworld.sql

--- SELECTworld.sql
+++ SELECTworld.sql
@@ -47,4 +47,14 @@ SELECT name, population, area
 FROM world
 WHERE area > 3000000 XOR population > 250000000;
 -- NOTE: Microsoft SQL does not recognize XOR
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 AND population < 250000000 
+OR area < 3000000 AND population > 250000000;
+
+-- 9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. 
+-- Use the ROUND function to show the values to two decimal places.
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+-- Millions and billions
+-- Divide by 1000000 (6 zeros) for millions. Divide by 1000000000 (9 zeros) for billions.
 
