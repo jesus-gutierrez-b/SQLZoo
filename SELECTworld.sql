6ef7ac11f3500b0567dce424b2ffe2b144400087
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-04-13 10:49:29 -0700

    Update SELECTworld.sql

--- SELECTworld.sql
+++ SELECTworld.sql
@@ -16,4 +16,30 @@ WHERE population > 200000000;
 
 -- 4. Show the name and population in millions for the countries of the continent 'South America'. 
 -- Divide the population by 1000000 to get population in millions.
+SELECT name, population/1000000 AS population
+FROM world
+WHERE continent = 'South America';
 
+-- 5. Show the name and population for France, Germany, Italy
+SELECT name, population
+FROM world
+WHERE name IN ('France', 'Germany', 'Italy');
+
+-- 6. Show the countries which have a name that includes the word 'United'
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
+
+-- 7. Two ways to be big: 
+-- A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+-- Show the countries that are big by area or big by population. Show name, population and area.
+SELECT name, population, area 
+FROM world 
+WHERE area > 3000000 OR population > 250000000;
+
+-- 8. Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. 
+-- Show name, population and area.
+-- Australia has a big area but a small population, it should be included.
+-- Indonesia has a big population but a small area, it should be included.
+-- China has a big population and big area, it should be excluded.
+-- United Kingdom has a small population and a small area, it should be excluded.
