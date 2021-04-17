0e232ef65c5644425d901a0d3c043ac820139a24
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-04-17 12:48:49 -0700

    Update SELECTwithinSELECT.sql

--- SELECTwithinSELECT.sql
+++ SELECTwithinSELECT.sql
@@ -16,3 +16,37 @@ AND gdp/population >
 
 -- 3. List the name and continent of countries in the continents containing either Argentina or Australia. 
 -- Order by name of the country.
+SELECT name, continent
+FROM world
+WHERE continent IN (SELECT continent FROM world WHERE name = 'Argentina' OR name ='Australia')
+ORDER BY name;
+
+-- 4. Which country has a population that is more than Canada but less than Poland? 
+-- Show the name and the population.
+SELECT name, population 
+FROM world
+WHERE population > (SELECT population FROM world WHERE name = 'Canada') 
+AND population < (SELECT population FROM world WHERE name = 'Poland');
+
+-- 5. Germany (population 80 million) has the largest population of the countries in Europe. 
+-- Austria (population 8.5 million) has 11% of the population of Germany.
+-- Show the name and the population of each country in Europe. 
+-- Show the population as a percentage of the population of Germany.
+-- The format should be Name, Percentage for example:
+-- name	percentage
+-- Albania	3%
+-- Andorra	0%
+-- Austria	11%
+-- ...	...
+-- Decimal places
+-- You can use the function ROUND to remove the decimal places.
+-- Percent symbol %
+-- You can use the function CONCAT to add the percentage symbol.
+SELECT name, CONCAT(
+                    ROUND(population/(SELECT population FROM world WHERE name = 'Germany')*100, 0), 
+                    '%') AS percentage
+FROM world
+WHERE continent = 'Europe';
+
+-- 6. Which countries have a GDP greater than every country in Europe? [Give the name only.] 
+-- (Some countries may have NULL gdp values)
