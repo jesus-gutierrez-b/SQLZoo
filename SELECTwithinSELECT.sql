e9a6d0f00b33b713015d909d404e57bb34fe89df
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-05-29 19:18:14 -0700

    Update SELECTwithinSELECT.sql
    Finished all exercises.

--- SELECTwithinSELECT.sql
+++ SELECTwithinSELECT.sql
@@ -84,3 +84,8 @@ WHERE 25000000 >= ALL(SELECT population
 
 -- 10. Some countries have populations more than three times that of any of their neighbours (in the same continent). 
 -- Give the countries and continents.
+SELECT name, continent
+FROM world a
+WHERE a.population > ALL (SELECT population*3 FROM world b 
+WHERE a.continent = b.continent
+AND a.name != b.name AND b.population > 0);
