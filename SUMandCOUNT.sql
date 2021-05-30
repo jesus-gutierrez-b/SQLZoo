00ea1e36540fffb64b5194ae73168402022adb19
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-05-29 22:08:49 -0700

    Update SUMandCOUNT.sql

--- SUMandCOUNT.sql
+++ SUMandCOUNT.sql
@@ -1,3 +1,9 @@
 -- 1. Show the total population of the world.
--- SELECT SUM(population)
--- FROM world;
+SELECT SUM(population)
+FROM world;
+
+-- 2. List all the continents - just once each.
+SELECT DISTINCT continent
+FROM world;
+
+-- 3. Give the total GDP of Africa
