73725f16227249fc84f0760b05f44cb7d6834693
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-05-31 12:58:00 -0700

    Update SUMandCOUNT.sql
    Finished all exercises.

--- SUMandCOUNT.sql
+++ SUMandCOUNT.sql
@@ -33,4 +33,7 @@ WHERE population >= 10000000
 GROUP BY continent;
 
 -- 8. List the continents that have a total population of at least 100 million.
-
+SELECT DISTINCT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >= 100000000;
