6c2306e2567ac751721848639cf3d2ea5ca8ac53
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-08 16:05:15 -0700

    Update JOIN.sql
    Finished all exercises.

--- JOIN.sql
+++ JOIN.sql
@@ -110,4 +110,11 @@ HAVING goals > 0;
 -- ...
 -- Notice in the query given every goal is listed. If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. 
 -- You could SUM this column to get a count of the goals scored by team1. Sort your result by mdate, matchid, team1 and team2.
-
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id
+  GROUP BY mdate, matchid, team1, team2
+  ORDER BY mdate
