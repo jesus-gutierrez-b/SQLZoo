127861ad1416ea85431cf7acca9d48eded379f45
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-09 11:28:13 -0700

    Update MOREJOIN.sql

--- MOREJOIN.sql
+++ MOREJOIN.sql
@@ -10,4 +10,14 @@ WHERE title = 'Citizen Kane';
 
 -- 3. List all of the Star Trek movies, include the id, title and yr 
 -- (all of these movies include the words Star Trek in the title). Order results by year.
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr;
 
+-- 4. What id number does the actor 'Glenn Close' have?
+SELECT id 
+FROM actor
+WHERE name = 'Glenn Close';
+
+-- 5. What is the id of the film 'Casablanca'
