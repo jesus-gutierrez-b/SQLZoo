823ea79f9e3ce0b0376d7e4c3963ac27bc7a5354
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-10 16:38:11 -0700

    Update MOREJOIN.sql

--- MOREJOIN.sql
+++ MOREJOIN.sql
@@ -78,4 +78,14 @@ HAVING COUNT(title) > 2;
 -- Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
 
 -- Title is not a unique field, create a table of IDs in your subquery
+SELECT title, name
+FROM movie JOIN casting ON movie.id = movieid AND ord = 1
+           JOIN actor ON actor.id = actorid
+WHERE movie.id IN (
+      SELECT movieid FROM casting
+       WHERE actorid IN (
+         SELECT id FROM actor
+           WHERE name = 'Julie Andrews'));
+           
+-- 13. Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
 
