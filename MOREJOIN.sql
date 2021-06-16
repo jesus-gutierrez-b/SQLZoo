55e4eaa4cdca4d348c2142dfb5688f9f519bffa8
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-16 13:03:50 -0700

    Update MOREJOIN.sql

--- MOREJOIN.sql
+++ MOREJOIN.sql
@@ -95,4 +95,10 @@ GROUP BY name
 HAVING COUNT(name) >= 15;
 
 -- 14. List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+SELECT title, COUNT(actorid)
+FROM movie JOIN casting ON movie.id = movieid
+WHERE yr = 1978
+GROUP BY title
+ORDER BY cOUNT(actorid) DESC, title;
+
 
