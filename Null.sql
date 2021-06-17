779452e83614c7d86a62a42fd383443aa7fcd4c0
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-17 12:15:09 -0700

    Update Null.sql

--- Null.sql
+++ Null.sql
@@ -1,3 +1,13 @@
 -- 1. List the teachers who have NULL for their department.
 
 -- Why we cannot use =
+-- You might think that the phrase dept=NULL would work here but it doesn't - you can use the phrase dept IS NULL
+
+-- That's not a proper explanation.
+-- No it's not, but you can read a better explanation at Wikipedia:NULL.
+SELECT name
+FROM teacher 
+WHERE dept IS NULL;
+
+--2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+
