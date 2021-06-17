007514233bf0daf9be3312a74f785e3c938eece7
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-17 16:51:42 -0700

    Update Null.sql

--- Null.sql
+++ Null.sql
@@ -30,4 +30,6 @@ FROM teacher;
 
 -- 6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. 
 -- Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE(dept.name, 'None') 
+FROM teacher LEFT JOIN dept ON teacher.dept = dept.id;
 
