4bb8db72aa1fe756aca1993a90989afdcfbaa6e2
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-17 12:16:44 -0700

    Update Null.sql

--- Null.sql
+++ Null.sql
@@ -10,4 +10,9 @@ FROM teacher
 WHERE dept IS NULL;
 
 --2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+           
+-- 3. Use a different JOIN so that all teachers are listed.
 
