3cb07ec41f702ee3038805c6d63c1064154da41b
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-19 10:01:36 -0700

    Update Null.sql

--- Null.sql
+++ Null.sql
@@ -39,3 +39,9 @@ FROM teacher;
 
 -- 8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. 
 -- Use a RIGHT JOIN to ensure that the Engineering department is listed.
+SELECT dept.name, COUNT(teacher.name) AS staff
+FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept
+GROUP BY dept.name;
+
+-- 9. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+
