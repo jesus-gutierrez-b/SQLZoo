eb8a75052083ebf9e78a07a3c66f921814a4d537
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-19 12:19:53 -0700

    Create Numeric.sql

---
+++ Numeric.sql
@@ -0,0 +1,6 @@
+-- 1. The example shows the number who responded for:
+
+-- question 1
+-- at 'Edinburgh Napier University'
+-- studying '(8) Computer Science'
+-- Show the the percentage who STRONGLY AGREE
