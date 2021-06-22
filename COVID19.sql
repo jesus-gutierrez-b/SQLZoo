bc87f46e44ffd08bc143773882c0fc80e173c4de
Jesús Gutiérrez Barajas <jesus-gutierrez-b@users.noreply.github.com>
2021-06-22 14:42:16 -0700

    Update COVID19.sql

--- COVID19.sql
+++ COVID19.sql
@@ -27,4 +27,17 @@ ORDER BY whn;
 -- 3. The number of confirmed case is cumulative - but we can use LAG to recover the number of new cases reported for each day.
 
 --Show the number of new cases for each day, for Italy, for March.
+SELECT name, DAY(whn),
+   confirmed - LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn) AS 'new cases'
+ FROM covid
+WHERE name = 'Italy'
+AND MONTH(whn) = 3
+ORDER BY whn;
+
+-- 4. The data gathered are necessarily estimates and are inaccurate. 
+-- However by taking a longer time span we can mitigate some of the effects.
+
+-- You can filter the data to view only Monday's figures WHERE WEEKDAY(whn) = 0.
+
+-- Show the number of new cases in Italy for each week - show Monday only.
 
